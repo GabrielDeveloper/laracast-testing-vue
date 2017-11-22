8a8438a7ab1cdba73eb45163082cb24349c5b6af
Gabriel Goncalves <gabrielll_07@hotmail.com>
2017-11-22 19:41:26 -0200

    Episode 3

--- test/counter.spec.js
+++ test/counter.spec.js
@@ -14,18 +14,36 @@ describe ('Counter', () => {
         expect(wrapper.vm.count).toBe(0);
     });
 
-    it ('increment count when button is clicked', () => {
+    it ('increments the count when the increment button is clicked', () => {
         expect(wrapper.vm.count).toBe(0);
 
-        wrapper.find('button').trigger('click');
+        wrapper.find('.increment').trigger('click');
 
         expect(wrapper.vm.count).toBe(1);
+    });
+
+    it ('decrements the count when the decrement button is clicked', () => {
+        wrapper.setData({ count : 5 });
+
+        wrapper.find('.decrement').trigger('click');
+
+        expect(wrapper.vm.count).toBe(4);
+    });
+
+    it ('never goes bellow 0', () => {
+        expect(wrapper.vm.count).toBe(0);
+
+        expect(wrapper.find('.decrement').hasStyle('display', 'none')).toBe(true);
+
+        wrapper.setData({ count: 1 });
+
+        expect(wrapper.find('.decrement').hasStyle('display', 'none')).toBe(false);
     });
 
     it ('presents the current count', () => {
         expect(wrapper.find('.count').html()).toContain(0);
 
-        wrapper.find('button').trigger('click');
+        wrapper.find('.increment').trigger('click');
 
         expect(wrapper.find('.count').html()).toContain(1);
     });
